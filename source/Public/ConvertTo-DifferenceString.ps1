a769f23c314a42a3e1ba9ee22abeee0f9c8915c8
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-01-31 21:44:09 +0100

    Improve control character conversion in `ConvertTo-DifferenceString`

--- source/Public/ConvertTo-DifferenceString.ps1
+++ source/Public/ConvertTo-DifferenceString.ps1
@@ -79,7 +79,6 @@
 #>
 function ConvertTo-DifferenceString
 {
-    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseBOMForUnicodeEncodedFile', '', Justification = 'This file is not intended to be saved as a Unicode-encoded file even though it has unicode characters, if that is a problem that can be re-evaluated.')]
     [CmdletBinding()]
     [OutputType([System.String])]
     param
@@ -174,10 +173,6 @@ function ConvertTo-DifferenceString
     $diffHexArray = @()
     $diffCharArray = @()
 
-    # Escape $HighlightStart and $HighlightEnd for regex matching
-    $escapedHighlightStart = [regex]::Escape($HighlightStart)
-    $escapedHighlightEnd = [regex]::Escape($HighlightEnd)
-
     # Output the labels if NoLabels is not specified
     if (-not $NoLabels)
     {
@@ -200,7 +195,7 @@ function ConvertTo-DifferenceString
         {
             $refByte = $referenceBytes[$i]
             $refHex = '{0:X2}' -f $refByte
-            $refChar = [char]$refByte
+            $refChar = if ($refByte -lt 32) { [char]($refByte + 0x2400) } elseif ($refByte -eq 127) { [char]0x2421 } else { [char]$refByte }
         }
         else
         {
@@ -213,7 +208,7 @@ function ConvertTo-DifferenceString
         {
             $diffByte = $differenceBytes[$i]
             $diffHex = '{0:X2}' -f $diffByte
-            $diffChar = [char]$diffByte
+            $diffChar = if ($diffByte -lt 32) { [char]($diffByte + 0x2400) } elseif ($diffByte -eq 127) { [char]0x2421 } else { [char]$diffByte }
         }
         else
         {
@@ -228,28 +223,13 @@ function ConvertTo-DifferenceString
             $refChar = "$($HighlightStart)$refChar$($HighlightEnd)"
             $diffHex = "$($HighlightStart)$diffHex$($HighlightEnd)"
             $diffChar = "$($HighlightStart)$diffChar$($HighlightEnd)"
+
+            $isHighlighted = $true
         }
-
-        # Replace control characters with their Unicode representations in the output
-        $refChar = $refChar`
-            -replace "`0", '␀' `
-            -replace "`a", '␇' `
-            -replace "`b", '␈' `
-            -replace "`t", '␉' `
-            -replace "`f", '␌' `
-            -replace "`r", '␍' `
-            -replace "`n", '␊' `
-            -replace "(?!$($escapedHighlightStart))(?!$($escapedHighlightEnd))`e", '␛'
-
-        $diffChar = $diffChar `
-            -replace "`0", '␀' `
-            -replace "`a", '␇' `
-            -replace "`b", '␈' `
-            -replace "`t", '␉' `
-            -replace "`f", '␌' `
-            -replace "`r", '␍' `
-            -replace "`n", '␊' `
-            -replace "(?!$($escapedHighlightStart))(?!$($escapedHighlightEnd))`e", '␛'
+        else
+        {
+            $isHighlighted = $false
+        }
 
         # Add to arrays
         $refHexArray += $refHex
@@ -283,8 +263,8 @@ function ConvertTo-DifferenceString
             $diffHexLine = ($diffHexArray -join ' ')
             $diffCharLine = ($diffCharArray -join '')
 
-            # Determine if the line was highlighted
-            $indicator = if ($refHexLine -match $escapedHighlightStart -or $diffHexLine -match $escapedHighlightStart)
+            # Output indicator depending if the line was highlighted or not.
+            $indicator = if ($isHighlighted)
             {
                 $NotEqualIndicator
             }
