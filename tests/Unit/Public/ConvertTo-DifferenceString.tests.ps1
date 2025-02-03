b2e56d3676ebe2f3e3051063620a58e97a625f5d
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-02-03 21:44:15 +0100

    Fix unit test

--- tests/Unit/Public/ConvertTo-DifferenceString.tests.ps1
+++ tests/Unit/Public/ConvertTo-DifferenceString.tests.ps1
@@ -155,4 +155,11 @@ Describe '-join (ConvertTo-DifferenceString' {
         $result | Should -Match "`e\[31m0D`e\[0m `e\[31m0A`e\[0m"  # Carriage return + Newline
         $result | Should -Match "`e\[31m0A`e\[0m `e\[31m0D`e\[0m"  # Newline + Carriage return
     }
+
+    It 'Should process large strings efficiently' {
+        $largeString1 = 'a' * 10000
+        $largeString2 = 'a' * 9999 + 'b'
+
+        Measure-Command { ConvertTo-DifferenceString -ReferenceString $largeString1 -DifferenceString $largeString2 } | Should-BeFasterThan '1.5s' -Because 'Large strings should be processed efficiently, and should not take more than 1.5 seconds.'
+    }
 }
