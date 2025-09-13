f4e9da1224a3f6f3c1483dec8a2c28e3f2793d02
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-09-13 20:47:18 +0200

    Add UTF-8 BOM requirement for Out-Difference tests and update .gitattributes

--- tests/Unit/Public/Out-Difference.tests.ps1
+++ tests/Unit/Public/Out-Difference.tests.ps1
@@ -1,4 +1,4 @@
-[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
+﻿[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
 param ()
 
 # ----
