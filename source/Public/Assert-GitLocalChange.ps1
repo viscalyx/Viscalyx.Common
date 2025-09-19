26cc5492c595ca639e61102ae08e970bc2e59c28
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-09-19 14:50:56 +0200

    Enhance Assert-GitLocalChange function with detailed input/output documentation and replace error handling with ThrowTerminatingError method; add integration and unit tests for comprehensive coverage of local change scenarios.

--- source/Public/Assert-GitLocalChange.ps1
+++ source/Public/Assert-GitLocalChange.ps1
@@ -7,31 +7,44 @@
         or staged changes in the local Git branch. If there are any staged or
         unstaged changes, it throws a terminating error.
 
+    .INPUTS
+        None
+
+        This function does not accept pipeline input.
+
+    .OUTPUTS
+        None
+
+        This function does not return any output.
+
     .EXAMPLE
         Assert-GitLocalChange
 
         This example demonstrates how to use the Assert-GitLocalChange command
         to ensure that there are no local changes in the Git repository.
+
+    .NOTES
+        This function requires Git to be installed and accessible from the command line.
+        The function will throw a terminating error if any staged or unstaged changes
+        are detected in the repository.
 #>
 function Assert-GitLocalChange
 {
     [CmdletBinding()]
+    [OutputType()]
     param ()
-
-    # Change the error action preference to always stop the script if an error occurs.
-    $ErrorActionPreference = 'Stop'
 
     $hasChanges = Test-GitLocalChanges
 
     if ($hasChanges)
     {
-        $errorMessageParameters = @{
-            Message = $script:localizedData.Assert_GitLocalChanges_FailedUnstagedChanges
-            Category = 'InvalidResult'
-            ErrorId = 'AGLC0001' # cspell: disable-line
-            TargetObject = 'Staged or unstaged changes' # cSpell: ignore unstaged
-        }
-
-        Write-Error @errorMessageParameters
+        $PSCmdlet.ThrowTerminatingError(
+            [System.Management.Automation.ErrorRecord]::new(
+                $script:localizedData.Assert_GitLocalChanges_FailedUnstagedChanges,
+                'AGLC0001', # cspell: disable-line
+                [System.Management.Automation.ErrorCategory]::InvalidResult,
+                'Staged or unstaged changes' # cSpell: ignore unstaged
+            )
+        )
     }
 }
